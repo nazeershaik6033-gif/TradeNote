01a6519530103085da4bd920d3702bb23aad5886
nazeershaik6033-gif <nazeershaik6033@gmail.com>
2026-05-26 13:17:28 +0530

    Add full TradeNote v19.0.3 source files (batch 1)

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,7 @@
 <script setup>
-import { computed } from 'vue'
-import { useRoute } from 'vue-router'
-
-const route = useRoute()
-
-const layout = computed(() => route.meta?.layout)
 </script>
-
 <template>
-  <component :is="layout">
-    <router-view />
-  </component>
+    <component :is="$route.meta.layout || 'div'">
+        <RouterView />
+    </component>
 </template>
